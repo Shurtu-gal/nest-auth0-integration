7f946e24c36fab3d8dde0465a018073a58c94885
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-09-10 20:23:19 +0530

    fix: auth in client

--- client/src/auth-provider/ra-auth-auth0.ts
+++ client/src/auth-provider/ra-auth-auth0.ts
@@ -1,56 +1,100 @@
-import { Auth0Client } from "@auth0/auth0-spa-js";
-import { AuthProvider } from "react-admin";
+import { Auth0Client } from '@auth0/auth0-spa-js';
+import {
+  AuthProvider,
+  PreviousLocationStorageKey,
+  UserIdentity,
+} from 'react-admin';
 
 const client = new Auth0Client({
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-  cacheLocation: "localstorage",
+  cacheLocation: 'localstorage',
   authorizationParams: {
     audience: import.meta.env.VITE_AUTH0_AUDIENCE,
   },
   useRefreshTokens: true,
 });
 
-export const Auth0AuthProvider : AuthProvider = {
+export const Auth0AuthProvider: AuthProvider = {
   login: async () => {
-    client.loginWithRedirect({
+    await client.loginWithRedirect({
       authorizationParams: {
-          redirect_uri: import.meta.env.VITE_REDIRECT_URI,
+        redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI,
       },
     });
+
+    return Promise.resolve();
   },
 
   logout: async () => {
-    return client.logout({
+    client.logout({
       logoutParams: {
         returnTo: import.meta.env.VITE_LOGOUT_REDIRECT_URI,
-      }
+      },
     });
+
+    return Promise.resolve();
   },
 
-  checkAuth: async () => {
+  async checkAuth() {
     const isAuthenticated = await client.isAuthenticated();
-    if (!isAuthenticated) {
-      throw new Error("Not authenticated");
+    if (isAuthenticated) {
+      return Promise.resolve();
     }
+
+    localStorage.setItem(PreviousLocationStorageKey, window.location.href);
+
+    return Promise.reject();
   },
 
   checkError: async ({ status }) => {
     if (status === 401 || status === 403) {
-        throw new Error('Unauthorized');
+      throw new Error('Unauthorized');
     }
   },
 
-  getPermissions: async() => {
+  getPermissions: async () => {
+    // if (!(await client.isAuthenticated())) {
+    //     return;
+    // }
+
+    // const claims = await client.getIdTokenClaims();
+    // console.log(claims);
+    Promise.resolve();
+  },
+
+  getIdentity: async () => {
     if (!(await client.isAuthenticated())) {
-        return;
+      throw new Error('User not authenticated');
     }
 
-    const claims = await client.getIdTokenClaims();
-    console.log(claims);
-    Promise.resolve();
-  }
+    const user = await client.getUser();
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return {
+      id: user.sub,
+      fullName: user.name,
+      avatar: user.picture,
+      email: user.email,
+    } as UserIdentity;
+  },
+
+  handleCallback: async () => {
+    const query = window.location.search;
+    if (query.includes('code=') && query.includes('state=')) {
+      // throw new Error('Failed to handle login callback.');
+      try {
+        const result = await client.handleRedirectCallback(window.location.href);
+        console.log(result);
+        localStorage.removeItem(PreviousLocationStorageKey);
+        return;
+      } catch (error) {
+        throw new Error('Failed to handle login callback: ' + error);
+      }
+    }
+    throw new Error('Failed to handle login callback.');
+  },
 };
-
-
-
--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -4,7 +4,7 @@ interface ImportMetaEnv {
   VITE_AUTH0_DOMAIN: string;
   VITE_AUTH0_CLIENT_ID: string;
   VITE_AUTH0_AUDIENCE: string;
-  VITE_REDIRECT_URI: string;
+  VITE_AUTH0_REDIRECT_URI: string;
   VITE_LOGOUT_REDIRECT_URI: string;
   VITE_API_URL: string;
 }
